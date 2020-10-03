7f57036e0405b143762eb802a7730f82aedba131
Nick Chadwick <nick.chadwick@capita.com>
2020-10-03 23:14:29 +0100

    Returns Trello to do items into App

--- README.md
+++ README.md
@@ -32,6 +32,8 @@ $ cp .env.template .env  # (first time only)
 
 The `.env` file is used by flask to set environment variables when running `flask run`. This enables things like development mode (which also enables features like hot reloading when you make a file change). There's also a [SECRET_KEY](https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY) variable which is used to encrypt the flask session cookie.
 
+Additional parameters to be populated in the configuration file include API_KEY, TOKEN, and TRELLO_BOARD. These parameters are used by the application to target a particular TRELLO board for maintaining the to_do items.
+
 ## Running the App
 
 Once the all dependencies have been installed, start the Flask app in development mode within the poetry environment by running:
--- todo_app/app.py
+++ todo_app/app.py
@@ -2,16 +2,28 @@ from flask import Flask, request, render_template
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items, add_item, get_item, save_item, delete_item
 from operator import itemgetter
+import requests
+import json
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+def get_status_name(card_id, authorisation):
+    status_name = requests.get('https://api.trello.com/1/cards/' + card_id +'/list', params = authorisation)
+    status_name_json = status_name.json()
+    return status_name_json["name"]
 
 @app.route('/')
 def index():
-    items = get_items()
-    ##renders the template with the items sorted by status
-    return render_template('index.html',items=sorted(items,key=itemgetter('status'), reverse=True))
+    trello_authorisation = {'key': Config.API_KEY,'token': Config.TOKEN}
+    cards_on_a_board = requests.get('https://api.trello.com/1/boards/' + Config.TRELLO_BOARD + '/cards', params = trello_authorisation)
+    cards_on_a_board_json = cards_on_a_board.json()
+        
+    for myitem in cards_on_a_board_json:
+        card_status_name = get_status_name(myitem["id"],trello_authorisation) 
+        myitem["status"] = card_status_name
+        
+    return render_template('index.html', items=cards_on_a_board_json)
 
 @app.route('/add_todo_item', methods=['POST']) 
 def add_todo_item(): 
