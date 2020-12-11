9b2cfcec500475a84a611e0b8c7f9e421206e81d
Nick <nick.chadwick@capita.com>
2020-12-11 12:02:40 +0000

    adding tests 2

--- tests/test_item_class.py
+++ tests/test_item_class.py
@@ -2,17 +2,18 @@
 
 import pytest
 from flask import Flask, request, render_template, redirect, flash
+import requests
+import json
 from todo_app.list_class import get_starting_list_id, add_card_to_list
-from todo_app.item_class import item, get_current_date
+from todo_app.item_class import item, get_card_object, get_items_on_a_board,get_list_progress,check_if_task_recently_completed, get_current_date
 
 def test_always_passes():
     assert True
 
 def test_get_current_date():
-    cards_on_a_board = get_items_on_a_board()
-    cards_on_a_board_json = cards_on_a_board.json()        
-    my_card_instance = []
-    number_of_filtered_items = 0
+    current_date = get_current_date()
+    assert len(current_date) > 9
+
 
 
 
