891f0f0053ed1910fab7e1084cdc7e878d5750f0
Nick <nick.chadwick@capita.com>
2020-12-19 18:51:01 +0000

    some integration tests added

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -1,20 +1,21 @@
 import os
+from dotenv import find_dotenv, load_dotenv
 
+class Config():
 
-class Config:
-    """Base configuration variables."""
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    if not SECRET_KEY:
-        raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
+        """Base configuration variables."""
+        SECRET_KEY = os.environ.get('SECRET_KEY')
+        if not SECRET_KEY:
+            raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
 
-    API_KEY = os.environ.get('API_KEY')
-    if not API_KEY:
-        raise ValueError("No API_KEY set for Flask application. Did you follow the setup instructions?")
-    
-    TOKEN = os.environ.get('TOKEN')
-    if not TOKEN:
-        raise ValueError("No TOKEN set for Flask application. Did you follow the setup instructions?")
+        API_KEY = os.environ.get('API_KEY')
+        if not API_KEY:
+            raise ValueError("No API_KEY set for Flask application. Did you follow the setup instructions?")
+        
+        TOKEN = os.environ.get('TOKEN')
+        if not TOKEN:
+            raise ValueError("No TOKEN set for Flask application. Did you follow the setup instructions?")
 
-    TRELLO_BOARD = os.environ.get('TRELLO_BOARD')
-    if not TRELLO_BOARD:
-        raise ValueError("No TRELLO_BOARD set for Flask application. Did you follow the setup instructions?")
+        TRELLO_BOARD = os.environ.get('TRELLO_BOARD')
+        if not TRELLO_BOARD:
+            raise ValueError("No TRELLO_BOARD set for Flask application. Did you follow the setup instructions?")
